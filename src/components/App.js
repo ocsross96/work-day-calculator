53f86500cd8458f7fc0bce0d8dd2e23a8d1d96d1
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-17 21:07:06 +0000

    Added bootstrap classes to App component

--- src/components/App.js
+++ src/components/App.js
@@ -8,11 +8,15 @@ export default class App extends Component {
 
   render() {
     return (
-      <div>
-        Work day calculator
-
-        <div>
-          <Form />
+      <div className="container pt-3">
+        <div className="row">
+          <div className="col-sm-10 offset-sm-1">
+            <div className="jumbotron">
+              <h1>Work Day Calculator</h1>
+              <p className="lead">Use our quick and easy tool to calculate what time you will finish work.</p>
+            </div>
+            <Form />
+          </div>
         </div>
       </div>
     );
