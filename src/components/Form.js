8adaefdb3b5dcd8e2f3c658310641773d7e53281
Ross Matthews <ross.matthews@ratedpeople.com>
2018-01-25 22:51:51 +0000

    Added select elements for time pickers

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,22 +1,48 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Input } from './FormElements';
+import moment from 'moment';
+import { Input, TimeSelect } from './FormElements';
 import { setStartTime, setBreakDuration, setWorkingHours } from '../actions/workDay';
 
 export class Form extends Component {
   constructor (props) {
     super(props);
 
-    // this.state = { 
-    //   startTime: props.startTime,
-    //   breakDuration: props.breakDuration,
-    //   workingHours: props.workingHours
-    // };
+    this.state = { 
+      startTime: props.startTime,
+      breakDuration: props.breakDuration,
+      workingHours: props.workingHours
+    };
 
     // console.log(this.state);
-
     this.handleInputChange = this.handleInputChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  getHours (maxHours) {
+    let hours = [];
+    for (var i = 0; i <= maxHours; i++) {
+      hours.push(i < 10 ? '0' + i : '' + i);
+    }
+    return hours;
+  }
+
+  getMinutes (divisor) {
+    let minutes = [];
+    for (var i = 0; i < 60; i++) {
+      if (i % divisor === 0) {
+        minutes.push(i < 10 ? '0' + i : '' + i);
+      }
+    }
+    return minutes;
+  }
+
+  getFinishTime () {
+    const startTime = this.state.startTime;
+    const breakDuration = this.state.breakDuration;
+    const workingHours = this.state.workingHours;
+
+    console.log('get finish time');
   }
 
   handleInputChange (ev) {
@@ -50,6 +76,32 @@ export class Form extends Component {
     }
   }
 
+  handleSelectChange (ev) {
+    ev.preventDefault();
+    const target = ev.target;
+    const value = target.value;
+    const name = target.name;
+    const unit = ev.target.getAttribute('data-unit');
+
+    console.log(value);
+    console.log(typeof value);
+
+    switch (name) {
+      case 'startTime':
+        //this.props.onStartTimeChange(value);
+        break;
+      case 'breakDuration':
+        //this.props.onBreakDurationChange(value);
+        break;
+      case 'workingHours':
+        //this.props.onWorkingHoursChange(value);
+        break;
+      default:
+      // some default case here
+
+    }
+  }
+
   handleSubmit (ev) {
     ev.preventDefault();
     console.log(this.state);
@@ -63,10 +115,51 @@ export class Form extends Component {
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
-          <Input label="Start time" id="startTime" name="startTime" onChange={this.handleInputChange} value={this.props.startTime} />
-          <Input label="Break duration" id="breakDuration" name="breakDuration" onChange={this.handleInputChange} value={this.props.breakDuration} />
-          <Input label="Working hours" id="workingHours" name="workingHours" onChange={this.handleInputChange} value={this.props.workingHours} />
-          <button type="submit" className="btn btn-primary">Calculate finish time</button>
+          {/* <Input
+            type="time"
+            label="Start time" 
+            id="startTime" 
+            name="startTime" 
+            onChange={this.handleInputChange} 
+            value={this.props.startTime} 
+          /> */}
+          <TimeSelect
+            name="startTime"
+            title="Start time"
+            hours={this.getHours(24)}
+            minutes={this.getMinutes(15)}
+            onSelectChange={this.handleSelectChange}
+          />
+          <TimeSelect
+            name="breakDuration"
+            title="Break duration"
+            hours={this.getHours(5)}
+            minutes={this.getMinutes(15)}
+            onSelectChange={this.handleSelectChange}
+          />
+          <TimeSelect
+            name="workingHours"
+            title="Working hours"
+            hours={this.getHours(12)}
+            minutes={this.getMinutes(15)}
+            onSelectChange={this.handleSelectChange}
+          />
+          {/* <Input 
+            type="text"
+            label="Break duration" 
+            id="breakDuration" 
+            name="breakDuration" 
+            onChange={this.handleInputChange} 
+            value={this.props.breakDuration} 
+          />
+          <Input 
+            type="text"
+            label="Working hours" 
+            id="workingHours" 
+            name="workingHours" 
+            onChange={this.handleInputChange} 
+            value={this.props.workingHours} 
+          /> */}
         </form>
         <p>Your finish time is {this.props.finishTime}</p>
       </div>
--- src/components/FormElements.js
+++ src/components/FormElements.js
@@ -4,11 +4,42 @@ export const Input = (props) => {
   return (
     <div className="form-group">
       <label htmlFor={props.id}>{props.label}</label>
-      <input 
-        type="text"
+      <input
         className="form-control"
         {...props}
         />
+    </div>
+  );
+};
+
+export const TimeSelect = (props) => {
+  return (
+    <div>
+      <div className="row">
+        <div className="col-sm-12">
+          <p className="mb-0">{props.title}</p>
+        </div>
+      </div>
+      <div className="row">
+        <div className="col-sm-2">
+          <div className="form-group">
+            <select name={`${props.name}`} onChange={props.onSelectChange} className="form-control" data-unit="hours">
+              {props.hours.map((hour, i) => {
+                return <option key={i} value={hour}>{hour}</option>
+              })}
+            </select>
+          </div>
+        </div>
+        <div className="col-sm-2">
+          <div className="form-group">
+            <select name={`${props.name}`} onChange={props.onSelectChange} className="form-control" data-unit="minutes">
+              {props.minutes.map((minute, i) => {
+                return <option key={i} value={minute}>{minute}</option>
+              })}
+            </select>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
